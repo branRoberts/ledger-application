617f37966208269a17c5bae31a3c075eea467159
branRoberts <brandonkrob12@gmail.com>
2026-04-27 11:52:06 -0400

    Fourth Commit added the constuctor and the getters and setters for the Transaction class

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,7 +1,60 @@
 package com.pluralsight;
 
-public class Transaction {
-    private String date;
-    private String time;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private String name;
+    private String brand;
+    private double amount;
+
+    public Transaction(LocalDate date, LocalTime time, String name, String brand, double amount) {
+        this.date = date;
+        this.time = time;
+        this.name = name;
+        this.brand = brand;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
